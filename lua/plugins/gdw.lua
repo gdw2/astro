b932a74f9caf98aa3b1ac38ff03ae84352ca66c9
Gregory Warner <gregory.warner@viasat.com>
2024-12-23 14:10:20 -0700

    fix copilot

--- lua/plugins/gdw.lua
+++ lua/plugins/gdw.lua
@@ -44,6 +44,7 @@ return {
   { import = "astrocommunity.git.gitlinker-nvim" },
   { import = "astrocommunity.terminal-integration.toggleterm-manager-nvim" },
   { import = "astrocommunity.completion.copilot-cmp" },
+  { import = "astrocommunity.editing-support.copilotchat-nvim" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.motion.leap-nvim" },
@@ -82,50 +83,6 @@ return {
         },
       })
     end,
-    keys = {
-      {
-        "<leader>ccq",
-        function()
-          local input = vim.fn.input "Quick Chat: "
-          if input ~= "" then
-            require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
-          end
-        end,
-        desc = "CopilotChat - Quick chat",
-      },
-
-      -- Show help actions with telescope
-      {
-        "<leader>cch",
-        function()
-          local actions = require "CopilotChat.actions"
-          require("CopilotChat.integrations.telescope").pick(actions.help_actions())
-        end,
-        desc = "CopilotChat - Help actions",
-      },
-      -- Show prompts actions with telescope
-      {
-        "<leader>ccp",
-        function()
-          local actions = require "CopilotChat.actions"
-          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
-        end,
-        desc = "CopilotChat - Prompt actions",
-      },
-    },
-  },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
-    opts = {
-      debug = true, -- Enable debugging
-      -- See Configuration section for rest
-    },
-    --         -- See Commands section for default commands if you want to lazy load on them
   },
   {
     "AstroNvim/astrocore",

6dedfc0bbbc48dfcb55107137082c23ff20d663d
Gregory Warner <gregory.warner@viasat.com>
2024-12-23 20:41:26 +0000

    remove 'fd' as escape
    messages with words that have 'fd' in them, like confd

--- lua/plugins/user-practicalli.lua
+++ lua/plugins/user-practicalli.lua
@@ -46,21 +46,21 @@ return {
 
   -- ------------------------------------------
   -- `fd` alternative to the `ESC` key
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertCharPre",
-    opts = {
-      timeout = vim.o.timeoutlen,
-      default_mappings = true,
-      mappings = {
-        i = { f = { d = "<Esc>" } },
-        c = { f = { d = "<Esc>" } },
-        t = { f = { d = "<Esc>" } },
-        v = { f = { d = "<Esc>" } },
-        s = { f = { d = "<Esc>" } },
-      },
-    },
-  },
+  -- {
+  --   "max397574/better-escape.nvim",
+  --   event = "InsertCharPre",
+  --   opts = {
+  --     timeout = vim.o.timeoutlen,
+  --     default_mappings = true,
+  --     mappings = {
+  --       i = { f = { d = "<Esc>" } },
+  --       c = { f = { d = "<Esc>" } },
+  --       t = { f = { d = "<Esc>" } },
+  --       v = { f = { d = "<Esc>" } },
+  --       s = { f = { d = "<Esc>" } },
+  --     },
+  --   },
+  -- },
   -- ------------------------------------------
 
   -- ------------------------------------------
